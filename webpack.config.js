a4ce7c93bf48cb38c29e1bfb4f92da43a686cb62
Dmitry <luchare.dev@gmail.com>
2022-11-28 20:09:13 +1000

    Изменение структуры директорий, настройка вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,44 @@
 const path = require("path");
 
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const CSSExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     mode: process.env.NODE_ENV || "production",
 
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "public/scripts")
+        path: path.resolve(__dirname, "public/")
     },
     
+    devServer: {
+        static: "./public"
+    },
+
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [CSSExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
-    }
+    },
+
+    performance: {
+        hints: false,
+    }, 
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: "./src/images", to: "images"}
+            ]
+        }),
+        new HtmlPlugin({ 
+            template: "./src/index.html",
+        }),
+        new CSSExtractPlugin(),
+    ]
 }
